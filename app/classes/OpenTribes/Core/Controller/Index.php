6aec34cee0ff1787b036ccf771b1e7bc8c0a3e16
BlackScorp <>
2013-08-09 13:25:59 +0200

    enabled controller service provider

--- app/classes/OpenTribes/Core/Controller/Index.php
+++ app/classes/OpenTribes/Core/Controller/Index.php
@@ -4,10 +4,10 @@ namespace OpenTribes\Core\Controller;
 
 class Index{
     public function __construct($test) {
-        ;
+       echo '<pre>'.print_r($test,true).'</pre>' ;
     }
 
     public function indexAction(){
-        
+        return 'test';
     }
 }
